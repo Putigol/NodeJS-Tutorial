6823f27e195121b82e921d676cc08c8cac0ad60a
Nguyen Phuc Thinh <23521504@gm.uit.edu.vn>
2026-07-17 23:45:19 +0700

    Nodemon

--- src/server.js
+++ src/server.js
@@ -1,33 +1,22 @@
-// const http = require("http");
-// const hostname = "127.0.0.1";
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader("Content-Type", "text/plain");
-//   res.end("Hello World");
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
 require("dotenv").config();
 console.log("env :", process.env);
 const express = require("express"); //Common JS
 const app = express(); //app express
 
-const port = process.env.PORT || 8888; //port server (hardcode) .uat(user access testing) .prod
+const port = process.env.PORT || 8081; //port server (hardcode) .uat(user access testing) .prod
 
 const path = require("path");
 const hostname = process.env.HOSTNAME;
 //Config template engine
 //Khai báo nơi lưu trữ view
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 // Khai báo route
+app.get("/", (req, res) => {
+  res.send("Hi there");
+});
 app.get("/putigol", (req, res) => {
-  // res.send(`<h1>Hello World</h1>`);
-  res.render("sample.ejs");
+  res.render("sample");
 });
 
 app.listen(port, hostname, () => {
